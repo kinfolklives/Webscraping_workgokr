afde688029ca1f03e68d39ecb954df8f5511f381
Bigmac <Bigmac@Bigmacs-iMac.local>
2020-10-14 22:55:02 +0900

    working on macos

--- data/workscraping.py
+++ data/workscraping.py
@@ -1,51 +1,50 @@
 from pymongo import MongoClient
 import time
-import datetime
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import schedule
-import time
 
 def insertDB(data):
-        with MongoClient('mongodb://127.0.0.1:27017/') as client: 
+        with MongoClient('mongodb://127.0.0.1:27017/') as client:
                 myworkdb = client['mytest']
                 myworkdb.test.insert_one(data)
 
 def Scrap():
-        driver = webdriver.Chrome(executable_path='/home/rapa01/Documents/Develop/chromedriver')
-        url = "https://www.work.go.kr"
-        driver.get(url=url)
+        path = '/Users/Bigmac/Documents/Develop/ownproject/data/chromedriverm'
+        with webdriver.Chrome(executable_path=path) as driver:
+                url = "https://www.work.go.kr/empInfo/empInfoSrch/list/dtlEmpSrchList.do?keyword=ai"
+                # url = "https://www.work.go.kr"
+                driver.get(url=url)
 
-        time.sleep(5) 
+                time.sleep(5)
 
-        search_form = driver.find_element_by_name("topQuery")
-        search_box = search_form.find_elements_by_css_selector("#topQuery")
-        search_form.send_keys("AI")
-        search_bnt = driver.find_element_by_css_selector("#searchFrm > div.header-search > a").click()
-        
-        for i in range(0, 11):
-                data = {}
-                try:
-                        data['name'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(2) > a").text
-                        data['title'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(3) > div > div > a").text
-                        data['desc'] = driver.find_element_by_css_selector(f"#jobContLine{i}").text
-                        data['career'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(3) > div > p:nth-child(3) > em:nth-child(1)").text
-                        data['academic'] = driver.find_element_by_xpath(f'//*[@id="list{i}"]/td[3]/div/p[2]/em[1]').text
-                        data['payment'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(4) > div > p:nth-child(1) > strong").text
-                        data['location'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(3) > div > p:nth-child(3) > em:nth-child(3)").text
-                        data['d_day1'] = driver.find_element_by_css_selector(f"#dDayInfo{i}").text
-                        data['d_day2'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(5) > div > p:nth-child(2)").text
-                except Exception as e:
-                        pass
+                # search_form = driver.find_element_by_name("topQuery")
+                # search_box = search_form.find_elements_by_css_selector("#topQuery")
+                # search_form.send_keys("AI")
+                # search_bnt = driver.find_element_by_css_selector("#searchFrm > div.header-search > a").click()
                 
-                print(data)
-                insertDB(data)
-                driver.quit()
+                for i in range(0, 11):
+                        data = {}
+                        try:
+                                data['name'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(2) > a").text
+                                data['title'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(3) > div > div > a").text
+                                data['desc'] = driver.find_element_by_css_selector(f"#jobContLine{i}").text
+                                data['career'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(3) > div > p:nth-child(3) > em:nth-child(1)").text
+                                data['academic'] = driver.find_element_by_xpath(f'//*[@id="list{i}"]/td[3]/div/p[2]/em[1]').text
+                                data['payment'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(4) > div > p:nth-child(1) > strong").text
+                                data['location'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(3) > div > p:nth-child(3) > em:nth-child(3)").text
+                                data['d_day1'] = driver.find_element_by_css_selector(f"#dDayInfo{i}").text
+                                data['d_day2'] = driver.find_element_by_css_selector(f"#list{i} > td:nth-child(5) > div > p:nth-child(2)").text
+                        except Exception as e:
+                                pass
+                        
+                        print(data)
+                        # insertDB(data)
+                        driver.quit()
                 
                 
+Scrap()            
 # schedule.every(10).minutes.do(Scrap)
 # while True:
 #     schedule.run_pending()
 #     time.sleep(1)
-    
